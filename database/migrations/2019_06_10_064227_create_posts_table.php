8d5eb6dd2cc64e176c9da095e1033f0be5f400ca
Jcasselm <Jcasselm@citadel.edu>
2019-06-10 02:57:45 -0400

    added login files

--- database/migrations/2019_06_10_064227_create_posts_table.php
+++ database/migrations/2019_06_10_064227_create_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreatePostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string("password");
-            $table->string('type')->default('default');
+            $table->string('title');
+            $table->string('author');
+            $table->string('category');
+            $table->string('short_description');
+            $table->string('long_description');
+
             $table->timestamps();
         });
     }
@@ -29,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('posts');
     }
 }
